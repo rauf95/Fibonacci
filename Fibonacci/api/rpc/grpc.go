e382a25cd0c2213a4218fc742e3accc201c28e44
Rauf <79477491+rauf95@users.noreply.github.com>
2021-12-05 23:43:55 +0300

    Update grpc.go

--- Fibonacci/api/rpc/grpc.go
+++ Fibonacci/api/rpc/grpc.go
@@ -14,7 +14,7 @@ type api struct{}
 
 func (a *api) Fibonacci(_ context.Context, request *pb.FibonacciRequest) (*pb.FibonacciResponse, error) {
 	fibonacci := core.Fibonacci(int(request.Arg))
-//вызывается в сервер джрпсишном сервере
+
 	var res []*pb.FibonacciValue
 	for _, value := range fibonacci {
 		res = append(res, &pb.FibonacciValue{
@@ -22,14 +22,13 @@ func (a *api) Fibonacci(_ context.Context, request *pb.FibonacciRequest) (*pb.Fi
 			Value:  int64(value.Value),
 		})
 	}
-// конвертация типов
+
 	return &pb.FibonacciResponse{Result: res}, nil
 }
 
 // New creates and returns gRPC server.
 func New() *grpc.Server {
 	srv := grpc.NewServer()
-	// Регистрация твоего grpc сервера
 	pb.RegisterFibonacciServiceServer(srv, &api{})
 
 	return srv
